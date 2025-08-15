3cc8cb17a4b576e6be0515acb081975b7b1813d3
Parzival <srivichai95@gmail.com>
2025-08-16 02:29:50 +0700

    5. Update Footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="2025" fullName="Tinnaphat" studentId="670610695" />
         </div>
       </div>
     </div>
